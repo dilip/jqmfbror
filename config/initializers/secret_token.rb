63b6cc585ed5d23776085aebed3796231db8ab34
Dilip Joseph <dilipjoseph@ubuntu.(none)>
2011-07-10 14:25:57 -0700

    empty rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqmfbror::Application.config.secret_token = '4e91621156e34cf1f16ace5d82f9fc0a37e5dee75533cfc1500b5c67d63c8fb96c37efcd43ce2a5170fcde13658730884d13756ff73250e34260783295b985bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jqmfbror::Application.config.session_store :cookie_store, :key => '_jqmfbror_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jqmfbror::Application.config.session_store :active_record_store
